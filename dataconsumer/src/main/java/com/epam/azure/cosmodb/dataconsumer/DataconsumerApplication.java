4b15c9da657243e31293b177a171514caeb85919
Anuar_Nurmakanov <Anuar_Nurmakanov@epam.com>
2019-07-02 15:25:58 +0600

    Initial version of data producer service

---
+++ dataconsumer/src/main/java/com/epam/azure/cosmodb/dataconsumer/DataconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.epam.azure.cosmodb.dataconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataconsumerApplication.class, args);
+	}
+
+}
---
+++ dataproducer/src/main/java/com/epam/azure/cosmodb/dataproducer/DataproducerApplication.java
@@ -0,0 +1,13 @@
+package com.epam.azure.cosmodb.dataproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataproducerApplication.class, args);
+	}
+
+}
